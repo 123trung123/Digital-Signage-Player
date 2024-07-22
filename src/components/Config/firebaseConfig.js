37fbee8cc08008dedd2cee2c15dacfa3a85bd1e2
TranTrung2k3 <105001804+123trung123@users.noreply.github.com>
2024-07-22 20:09:05 +0700

    Update firebaseConfig.js

--- src/components/Config/firebaseConfig.js
+++ src/components/Config/firebaseConfig.js
@@ -3,18 +3,24 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyCf8c_lL6oQ6ShiHqrRVNT-w76ZIx7W178",
-    authDomain: "digitalsignage-21521774.firebaseapp.com",
-    databaseURL: "https://digitalsignage-21521774-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "digitalsignage-21521774",
-    storageBucket: "digitalsignage-21521774.appspot.com",
-    messagingSenderId: "1005072885556",
-    appId: "1:1005072885556:web:5b1b3984db9b5a393b6761",
-    measurementId: "G-JZWRR6STYE"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+//export cái firebaseConfig ra ngoài để App.jsx nhận
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export default firebaseConfig;
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
